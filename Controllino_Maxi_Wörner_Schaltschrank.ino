eff151d454a076c186e13ca2c05c18b63675323e
Alessandro Tellini <oreste.tellini@gmail.com>
2022-01-12 16:56:10 +0100

    Code Revision
    - Added Enum States
    - Renamed Functions
    - Simplified Main Loop Logic
    - Simplified ..._mode() functions for better readability

--- Controllino_Maxi_Wörner_Schaltschrank.ino
+++ Controllino_Maxi_Wörner_Schaltschrank.ino
@@ -1,30 +1,30 @@
 #include <SPI.h>
 #include <Controllino.h>
 
-//Emercency Stop - Input 
+//Emercency Stop - Input
 #define estop_pin CONTROLLINO_AI0
 
-//Contactor interupt - Input 
+//Contactor interupt - Input
 #define contactor_pin CONTROLLINO_AI1
 
-//Electrical Cabinet
-#define ec_reset_pin CONTROLLINO_DO3
-
-//Controlpanel - Input / Output 
+//Controlpanel - Input / Output
 #define cp_on_pin CONTROLLINO_AI3
 #define cp_off_pin CONTROLLINO_AI4
 #define cp_cw_pin CONTROLLINO_AI5
 #define cp_ccw_pin CONTROLLINO_AI6
 #define cp_mode_pin CONTROLLINO_AI7 //mode selector -> drilling or tapping
-#define cp_speed_pin CONTROLLINO_AI8 //analog input 10k pot
-#define cp_indicator_pin CONTROLLINO_DO3 //output -> led
+#define cp_speed_pin CONTROLLINO_AI12 //analog input 4.7k pot
+#define cp_indicator_pin CONTROLLINO_DO0 //output -> led
 
-//Wörner endstops for tapping - Input 
+//Electrical Cabinet - Input / Output
+#define ec_reset_led CONTROLLINO_DO3
+
+//Wörner endstops for tapping - Input
 #define w_topswitch_pin CONTROLLINO_AI9
 #define w_bottomswitch_pin CONTROLLINO_AI10
 #define w_footswitch_pin CONTROLLINO_AI11
 
-//Eaton DE1 Control- Input / Output 
+//Eaton DE1 Control- Input / Output
 #define vfd_state_pin CONTROLLINO_AI2 //input
 #define vfd_run_pin CONTROLLINO_DO0
 #define vfd_cw_pin CONTROLLINO_DO1
@@ -32,22 +32,32 @@
 #define vfd_speed_pin CONTROLLINO_AO0 // Analog out 0 - 0-10V
 
 //Variables
+int old_spindle_speed = 0;
 int spindle_speed = 0;
-int dir_change_delay = 100;
+int dir_change_delay = 10;
 int fs_state = 0; //footswitch
 int ts_state = 0; //topswitch
 int bs_state = 0; //bootomswitch
 int fs_last_state = 0; //footswitch
 int ts_last_state = 0; //topswitch
 int bs_last_state = 0; //bottomswich
-String spindle_dir = "CW"; //CW or CCW
+String tspindle_dir = "CCW"; //CW or CCW
+
+//STATES
+enum m_mode {DRILL, TAP, ESTOP};
+enum m_mode machine_mode;
+enum m_state {ON, OFF};
+enum m_state machine_state;
+enum s_dir {CW, CCW, NONE};
+enum s_dir spindle_direction;
+
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(19200);
   //e-stop, contactor, reset
   pinMode(estop_pin, INPUT);
   pinMode(contactor_pin, INPUT);
-  pinMode(ec_reset_pin, OUTPUT);
+  pinMode(ec_reset_led, OUTPUT);
 
   //control panel
   pinMode(cp_on_pin, INPUT);
@@ -72,141 +82,190 @@ void setup() {
 
   //Waiting for ESTOP, CONTACTOR and VFD to signal all good!
   Serial.println("INIT...");
-  while(digitalRead(estop_pin) == LOW && digitalRead(contactor_pin) == LOW && digitalRead(vfd_state_pin) == LOW){
+  while (digitalRead(estop_pin) == LOW && digitalRead(contactor_pin) == LOW && digitalRead(vfd_state_pin) == LOW) {
     Serial.println("waiting for e_stop, contactor and or vfd to start operation!");
     delay(250);
   }
-  Serial.println("READY!");
+  Serial.println("MACHINE READY!");
+  
+  //make sure spindle is off
+  machine_state = OFF;
 }
 
 void loop() {
-  //see if estop is active and choose drill or tap mode
-  if(estop_checker() == false && drill_or_tap() == false){
-    drilling_mode();
-  }
-  if(estop_checker() == false && drill_or_tap() == true){
-    tapping_mode();
-  }
-}
-
-bool estop_checker(){
-  //check estop pin
-  if(digitalRead(estop_pin) == HIGH){
-    digitalWrite(ec_reset_pin, HIGH);
-    return true;
-  }
-  if(digitalRead(estop_pin) == LOW){
-    digitalWrite(ec_reset_pin, LOW);
-    return false;
+  check_mode();
+  
+  switch (machine_mode) {
+    case DRILL:
+      drilling_mode();
+      break;
+    case TAP:
+      tapping_mode();
+      break;
+    case ESTOP:
+      estop_mode();
+      break;
+    default:
+      break;
   }
 }
 
-bool drill_or_tap() {
-  //check drill or tap mode pin
-  if(digitalRead(cp_mode_pin) == HIGH){
-    return true;
-  }else{
-    return false;
+void check_mode() {
+  //check drill, tap switch and estop and reset state
+  if (digitalRead(estop_pin) == HIGH) {
+    machine_mode = ESTOP;
+  }
+  if (digitalRead(estop_pin) == LOW && digitalRead(cp_mode_pin) == HIGH) {
+    machine_mode = TAP;
+  }
+  if (digitalRead(estop_pin) == LOW &&  digitalRead(cp_mode_pin) == LOW) {
+    machine_mode = DRILL;
   }
 }
 
-void run() {
-  //run the spindle 
-  if(digitalRead(cp_on_pin) == HIGH && digitalRead(cp_on_pin) == LOW){
-    digitalWrite(vfd_run_pin, HIGH); //spindle on
+void run_machine() {
+  check_onoff();
+  //turn machine on or off
+  switch (machine_state) {
+    case ON:
+      set_spindle_speed();
+      if(machine_mode == DRILL){
+        get_spindle_dir();
+        set_spindle_dir();
+      }
+      if(machine_mode == TAP){
+        if(spindle_direction == NONE){
+          spindle_direction = CW;
+        }
+        get_tapping_dir();
+        set_spindle_dir();
+      }
+      digitalWrite(vfd_run_pin, HIGH);
+      break;
+    case OFF:
+      spindle_direction = NONE;
+      set_spindle_dir();
+      digitalWrite(vfd_run_pin, LOW);
+      break;
+    default:
+      break;
   }
-  if(digitalRead(cp_on_pin) == LOW && digitalRead(cp_on_pin) == HIGH){
-    digitalWrite(vfd_run_pin, HIGH);
+}
+
+void check_onoff() {
+  //check input panel to see if the user turns the machine on or off
+  if (digitalRead(cp_on_pin) == HIGH && digitalRead(cp_off_pin) == LOW) {
+    machine_state = ON;
+  }
+  if (digitalRead(cp_on_pin) == LOW && digitalRead(cp_off_pin) == HIGH) {
+    machine_state = OFF;
   }
 }
 
 
-void set_spindle_direction(String dir) {
-  //change spindle direction depending on dir string
-  if(dir == "CW"){
-    digitalWrite(vfd_ccw_pin, LOW);
-    delay(dir_change_delay);
-    digitalWrite(vfd_cw_pin, HIGH);
-  }
-  if(dir == "CCW"){
-    digitalWrite(vfd_cw_pin, LOW);
-    delay(dir_change_delay);
-    digitalWrite(vfd_ccw_pin, HIGH);
+void set_spindle_dir() {
+  //set direction output pins
+  switch (spindle_direction) {
+    case CW:
+      digitalWrite(vfd_ccw_pin, LOW);
+      digitalWrite(vfd_cw_pin, HIGH);
+      break;
+    case CCW:
+      digitalWrite(vfd_ccw_pin, HIGH);
+      digitalWrite(vfd_cw_pin, LOW);
+      break;
+    case NONE:
+      digitalWrite(vfd_cw_pin, LOW);
+      digitalWrite(vfd_ccw_pin, LOW);
+      break;
+    default:
+      break;
   }
 }
 
-String get_spindle_dir(){
+void get_spindle_dir() {
   //get the current spindle direction
-  if(digitalRead(cp_cw_pin) == HIGH && digitalRead(cp_ccw_pin) == LOW){
-    return "CCW";
+  if (digitalRead(cp_cw_pin) == LOW && digitalRead(cp_ccw_pin) == HIGH) {
+    spindle_direction = CCW;
   }
-  if(digitalRead(cp_cw_pin) == LOW && digitalRead(cp_ccw_pin) == HIGH){
-    return "CW";
+  if (digitalRead(cp_cw_pin) == HIGH && digitalRead(cp_ccw_pin) == LOW) {
+    spindle_direction = CW;
+  }
+  if (digitalRead(cp_cw_pin) == LOW && digitalRead(cp_ccw_pin) == LOW) {
+    spindle_direction = NONE;
   }
 }
 
 void set_spindle_speed() {
-  //set spindle speed acording to pot valie
+  //set spindle speed acording to pot value
   spindle_speed = analogRead(cp_speed_pin);
-  analogWrite(vfd_speed_pin, spindle_speed);
+  int speed_value = int(map(spindle_speed, 0, 1025, 30, 255));
+  analogWrite(vfd_speed_pin, speed_value);
 }
 
-void tap_spindle_direction(){
-  //change spindle direction depending on the state of the inputs
-  fs_state = digitalRead(w_footswitch_pin);
+void get_tapping_dir() {
+  //invert spindle direction when either top or bottom endstop are pressed
   bs_state = digitalRead(w_bottomswitch_pin);
   ts_state = digitalRead(w_topswitch_pin);
-
-  //check bottom switch state
-  if(bs_state != bs_last_state){
-    if(bs_state == HIGH){
-      if(get_spindle_dir() == "CW"){
-        set_spindle_direction("CCW");
-      }
-      if(get_spindle_dir() == "CCW"){
-        set_spindle_direction("CW");
-      }
+  fs_state = digitalRead(w_footswitch_pin);
+  //check bottom switch
+  if (bs_state != bs_last_state) {
+    if (bs_state == HIGH) {
+      spindle_direction = CCW;
     }
   }
-
-  //check foot switch state
-  if(fs_state != fs_last_state){
-    if(fs_state == HIGH){
-      if(get_spindle_dir() == "CW"){
-        set_spindle_direction("CCW");
-      }
-      if(get_spindle_dir() == "CCW"){
-        set_spindle_direction("CW");
-      }
+  //check top switch
+  if (ts_state != ts_last_state) {
+    if (ts_state == HIGH) {
+      spindle_direction = CW;
     }
-  }  
-
-  //check top switch state
-  if(ts_state != ts_last_state){
-    if(ts_state == HIGH){
-      if(get_spindle_dir() == "CW"){
-        set_spindle_direction("CCW");
-      }
-      if(get_spindle_dir() == "CCW"){
-        set_spindle_direction("CW");
-      }
+  }
+  //check foot switch
+  if (fs_state != fs_last_state) {
+    if (fs_state == HIGH && ts_state == LOW && bs_state == LOW) {
+      spindle_direction = CCW;
     }
   }
 
   fs_last_state = fs_state;
   bs_last_state = bs_state;
   ts_last_state = ts_state;
-
 }
 
-void drilling_mode(){
-  tap_spindle_direction();
-  set_spindle_speed();
-  run();
+void drilling_mode() {
+  Serial.println("DRILLING MODE");
+  machine_state = OFF;
+  while (true) {
+    check_mode();
+    run_machine();
+    if (machine_mode != DRILL) {
+      break;
+    }
+  }
 }
 
-void tapping_mode(){
-    tap_spindle_direction();
-    set_spindle_speed();
-    run();
+void tapping_mode() {
+  Serial.println("TAPPING MODE");
+  machine_state = OFF;
+  while (true) {
+    check_mode();
+    run_machine();
+    if (machine_mode != TAP) {
+      break;
+    }
+  }
+}
+
+void estop_mode() {
+  Serial.println("ESTOP MODE");
+  digitalWrite(ec_reset_led, HIGH);
+
+  while (true) { 
+    if (digitalRead(estop_pin) == LOW && digitalRead(contactor_pin) == HIGH) {
+      Serial.println("RESET PRESSED!");
+      digitalWrite(ec_reset_led, LOW);
+      machine_state = OFF;
+      break;
+    }
+  }
 }
